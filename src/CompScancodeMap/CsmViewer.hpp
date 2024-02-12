791435d79c75f18e8f423e512d8d4ff73d15b3bc
Y-T10 <t06t06ca2@gmail.com>
2024-02-12 18:41:37 +0900

    メンバ変数の並びを変更し、型の制約を追加

--- src/CompScancodeMap/CsmViewer.hpp
+++ src/CompScancodeMap/CsmViewer.hpp
@@ -1,6 +1,7 @@
 #pragma  once
 
 #include <cstdint>
+#include <type_traits>
 #include <vector>
 
 namespace CompScanMap {
@@ -8,10 +9,12 @@ namespace CompScanMap {
     using Scancode =uint16_t;
 
     /// スキャンコードの対応
+    /// コーデック処理を容易にするため、メンバの順番は入れ替えないこと．
     struct ConvertPair {
-        Scancode from;
         Scancode to;
+        Scancode from;
     };
+    static_assert(std::has_unique_object_representations_v<ConvertPair>);
 
     /// Scancodeの変換表
     using ConvertMap = std::vector<ConvertPair>;
