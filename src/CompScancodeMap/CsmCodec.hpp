5d7e808450a7004191af1e98f2583eac06b98eb9
Y-T10 <t06t06ca2@gmail.com>
2024-02-12 16:40:13 +0900

    ScancodeMapバイナリのコーデック関数の宣言を追加

---
+++ src/CompScancodeMap/CsmCodec.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <optional>
+#include <cstdint>
+#include <vector>
+
+#include "CsmViewer.hpp"
+
+namespace CompScanMap {
+    /**
+     * @brief バイナリ列から変換表を取り出す．
+     * @param bin `Scancode Map`のバイナリ列．
+     * @return 失敗時はnulloptを、成功時はオブジェクトを返す．
+     */
+    const std::optional<ConvertMap> DecodeScancodeMap(const std::vector<uint8_t>& bin) noexcept;
+
+    /**
+     * @brief 変換表からバイナリ列を生成する
+     * @param map 変換表
+     * @return 失敗時はnulloptを、成功時はオブジェクトを返す．
+     */
+    const std::optional<std::vector<uint8_t>> EncodeScancodeMap(const ConvertMap& map) noexcept;
+}
