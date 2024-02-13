9cc1d516f168a891decc0995f8377917a75813ec
Y-T10 <t06t06ca2@gmail.com>
2024-02-13 22:48:04 +0900

    レジストリの型とレジストリ操作関数の宣言を追加

---
+++ src/CompRegstry/CregHandler.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CregTypes.hpp"
+
+#include <optional>
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include <winreg.h>
+
+namespace CompReg {
+    /// HKEYはポインタ型
+    static_assert(std::is_pointer_v<HKEY>);
+    /// HKEYのデリータ
+    struct HKEY_deleter {
+        void operator()(HKEY ptr) {
+            RegCloseKey(ptr);
+        }
+    };
+    /// HKEYのユニークポインタ
+    using RegKey = std::unique_ptr<std::remove_pointer_t<HKEY>, HKEY_deleter>;
+
+    /**
+     * @brief レジストリのキーを開く
+     * @param rootKey 始点となるキー．
+     * @param keyPath rootKey下のキーを指すパス
+     * @param access キーを扱う際の権限
+     * @return 成功すればキーのハンドラを指すポインタ、失敗すればnullptrを返す．
+     */
+    const RegKey OpenHKLMKey(const HKEY rootKey, const win32str& keyPath, const REGSAM access) noexcept;
+
+    /**
+     * @brief キーが持つバイナリ値を読み込む
+     * @param key キー
+     * @param valueName 値の名前 
+     * @return 読み取りに成功すればバイナリ値を、失敗すればnulloptを返す．
+     */
+    const std::optional<std::vector<uint8_t>> ReadKeyValueBin(const RegKey& key, const win32str& valueName) noexcept;
+};
