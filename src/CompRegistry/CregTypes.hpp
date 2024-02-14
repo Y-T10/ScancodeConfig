3554b9304166794ce2ccd1ce0edd09d9236150fa
Y-T10 <t06t06ca2@gmail.com>
2024-02-14 11:10:57 +0900

    win32strの定義を変更

--- src/CompRegistry/CregTypes.hpp
+++ src/CompRegistry/CregTypes.hpp
@@ -1,13 +1,10 @@
 #pragma once
 
-#include <type_traits>
 #include <string>
 
 #include <windows.h>
 
 namespace CompReg {
-    /// Win32APIのChar型．
-    using win32api_char = std::remove_pointer_t<LPSTR>;
-    /// Win32APIのChar型を扱う文字列型．
-    using win32str = std::basic_string<win32api_char>;
+    /// Win32APIの文字型を扱う文字列型．
+    using win32str = std::basic_string<TCHAR>;
 };
