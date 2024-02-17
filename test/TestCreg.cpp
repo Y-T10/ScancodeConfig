b0e060fc407796a6304fa45bc24bbdb16cff25a5
Y-T10 <t06t06ca2@gmail.com>
2024-02-17 17:41:00 +0900

    レジストリの書き込みのテストを追加

--- test/TestCreg.cpp
+++ test/TestCreg.cpp
@@ -9,6 +9,36 @@
 
 using namespace CompReg;
 
+void TestRegWriteInternal(const std::vector<uint8_t>& value) noexcept {
+    constexpr auto Subkey = TEXT("SYSTEM\\CurrentControlSet\\Control\\Keyboard Layout");
+    constexpr auto ValueName = TEXT("Scancode Map");
+
+    // キーを生成する．パスのキーが存在しない場合にも対応する．
+    const auto ReadKey = OpenRegKey(
+        HKEY_LOCAL_MACHINE, Subkey, KEY_READ
+    );
+    const auto WriteKey = OpenRegKey(
+        HKEY_LOCAL_MACHINE, Subkey, KEY_SET_VALUE
+    );
+    CHECK(ReadKey);
+    CHECK(WriteKey);
+
+    // 既存の値を待避する
+    const auto ValOrigin = ReadKeyValueBin(ReadKey, ValueName);
+    CHECK(ValOrigin);
+
+    // データを書き込む
+    CHECK(WriteKeyValueBin(WriteKey, ValueName, value));
+    // 値を取り出す．
+    const auto readValue = ReadKeyValueBin(ReadKey, ValueName);
+    CHECK(readValue);
+    // 読んだ値と書いた値が一致しているかを調べる
+    CHECK(std::ranges::equal(*readValue, value));
+
+    // 既存のデータを元に戻す
+    CHECK(WriteKeyValueBin(WriteKey, ValueName, *ValOrigin));
+}
+
 TEST_CASE("Open registry key") {
     const auto readKey = OpenRegKey(
         HKEY_LOCAL_MACHINE,
@@ -55,4 +85,17 @@ TEST_CASE("Check Privilege") {
     );
     CHECK(writeKeySuccess);
     DisableWritingRegPriv();
+};
+
+#include "TestBinData.hpp"
+
+TEST_CASE("Write Scancode Map") {
+    if(const auto result = IsElevated();!result || !(*result)) {
+        MESSAGE("このテストを行う際は管理者権限が必要です．");
+        return;
+    }
+
+    TestRegWriteInternal(EmptyMap);
+    TestRegWriteInternal(ExaMap1);
+    TestRegWriteInternal(ExaMap2);
 };
