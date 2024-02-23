10a7328e018bb8a2c81c4efa99e8fdbd7c1ee2fe
Y-T10 <t06t06ca2@gmail.com>
2024-02-23 17:48:48 +0900

    内部関数の引数を変更

--- cmake/AddTargetHelper.cmake
+++ cmake/AddTargetHelper.cmake
@@ -21,35 +21,24 @@ function(set_target_build_internal TargetName)
     target_compile_options(${TargetName} PRIVATE $<$<BOOL:${Param_Debug}>:-g3>)
 endfunction()
 
-function(__add_obj_internal)
-    set(_obj_opt Debug EXE)
-    set(_obj_arg Directory)
-    set(_obj_varg ${_common_varg})
-    cmake_parse_arguments(arg "${_obj_opt}" "${_obj_arg}" "${_obj_varg}" ${ARGN})
-
-    get_filename_component(Name ${arg_Directory} NAME_WE)
-    if(${arg_EXE})
+function(__add_obj_internal Directory EXE)
+    get_filename_component(Name ${Directory} NAME_WE)
+    if(${EXE})
         add_executable(${Name})
     else()
         add_library(${Name})
     endif()
-    set_target_build_internal(
-        ${Name}
-        Debug ${arg_Debug}
-        SOURCE ${arg_SOURCE}
-        LIBPUB ${arg_LIBPUB}
-        LIBPRIV ${arg_LIBPRIV}
-    )
+    set_target_build_internal(${Name} ${ARGN})
 endfunction()
 
 ## ライブラリターゲットを追加する
 function(add_lib_directory Directory)
-    __add_obj_internal(Directory ${Directory} ${ARGN})
+    __add_obj_internal(${Directory} FALSE ${ARGN})
 endfunction(add_lib_directory)
 
 ## バイナリターゲットを追加する
 function(add_exe_directory Directory)
-    __add_obj_internal(Directory ${Directory} EXE ${ARGN})
+    __add_obj_internal(${Directory} TRUE ${ARGN})
 endfunction(add_exe_directory)
 
 ## インターフェイスターゲットを追加する
