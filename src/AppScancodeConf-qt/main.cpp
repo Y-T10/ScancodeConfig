2e8b890baed11e57701da1a47e0c6eee91dcb6e3
Y-T10 <t06t06ca2@gmail.com>
2024-03-12 11:23:30 +0900

    MainWindoのクラス定義をインクルードに置き換えた

--- src/AppScancodeConf-qt/main.cpp
+++ src/AppScancodeConf-qt/main.cpp
@@ -1,26 +1,9 @@
-#include <QItemSelection>
-#include <QProgressBar>
 #include <QApplication>
-#include <QPushButton>
-#include <QMainWindow>
-#include <QMenuBar>
 #include <QPointer>
-#include <QWidget>
-#include <QSlider>
 
-class MainWindow : public QMainWindow {
-    public:
-        explicit MainWindow() noexcept;
+#include "AsconfMainWindow.hpp"
 
-    private slots:
-        void updateActions(const QItemSelection &selection) noexcept;
-        void applyMapping() noexcept;
-        void importMapping() noexcept;
-        void exportMapping() noexcept;
-    
-    private:
-        void createMenu() noexcept;
-};
+using namespace AppSacnConf;
 
 int main(int argc, char* argv[]) {
     QPointer<QApplication> app (new QApplication(argc, argv));
@@ -28,40 +11,3 @@ int main(int argc, char* argv[]) {
     window.show();
     return app->exec();
 }
-
-MainWindow::MainWindow() noexcept:
-QMainWindow(){
-    createMenu();
-    setWindowTitle(tr("Scancode Map Viewer"));
-}
-
-void MainWindow::createMenu() noexcept {
-    auto fileMenu = menuBar()->addMenu(tr("&File"));
-
-    QAction* importAct = new QAction(tr("&Import Mapping"));
-    fileMenu->addAction(importAct);
-    connect(importAct, &QAction::triggered, this, &MainWindow::importMapping);
-
-    QAction* exportAct = new QAction(tr("&Export Mapping"));
-    fileMenu->addAction(exportAct);
-    connect(exportAct, &QAction::triggered, this, &MainWindow::exportMapping);
-
-    auto applyMenu = menuBar()->addMenu(tr("&Apply"));
-    QAction* applyAct = new QAction(tr("&Apply mapping"));
-    applyMenu->addAction(applyAct);
-    connect(applyAct, &QAction::triggered, this, &MainWindow::applyMapping);
-}
-
-void updateActions(const QItemSelection &selection) noexcept {
-
-}
-
-void MainWindow::applyMapping() noexcept {
-
-};
-void MainWindow::importMapping() noexcept {
-
-};
-void MainWindow::exportMapping() noexcept {
-
-};
