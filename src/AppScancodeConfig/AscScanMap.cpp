50e88aa74bc98058fb4dad0de07f9178522f6948
Y-T10 <t06t06ca2@gmail.com>
2024-03-14 23:40:51 +0900

    GTKを用いた実装を削除

--- src/AppScancodeConfig/AscScanMap.cpp
+++
@@ -1,8 +0,0 @@
-#include "AscScanMap.hpp"
-
-namespace AppScanConf {
-    ScanMapping::ScanMapping(const value_type& src, const value_type& dst) noexcept:
-    keySrc(src),
-    keyDst(dst){
-    };
-};
