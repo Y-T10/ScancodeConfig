23559e1b330edcdf584bb7c8f1d61fb3a1fc9214
Y-T10 <t06t06ca2@gmail.com>
2024-04-02 11:41:40 +0900

    パイプハンドラのラッパーを追加

--- src/CompProcess/CprocPipe.hpp
+++ src/CompProcess/CprocPipe.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
 #include "CregTypes.hpp"
+#include <memory>
+#include <type_traits>
 
 namespace CmpProc {
+    struct pipe_deleter {
+        void operator()(HANDLE h) noexcept {
+            CloseHandle(h);
+        }
+    };
+
+    /// パイプのハンドラをラップした型
+    using PipeHandle = std::unique_ptr<
+        std::remove_pointer_t<HANDLE>,
+        pipe_deleter
+    >;
+
     /**
      * @brief パイプの名前を生成する
      * @return パイプ名を持つ文字列
