29de39fd9cf20396ed3749c39c166d485685ee12
Y-T10 <t06t06ca2@gmail.com>
2024-05-13 15:38:36 +0900

    SDLとImGuiの初期化と終了を追加

--- src/AppScancodeConf-sdl/main.cpp
+++ src/AppScancodeConf-sdl/main.cpp
@@ -1,3 +1,5 @@
+#include "SDL_init.h"
+#include "SDL_hints.h"
 #include "SDL_render.h"
 #include "SDL_video.h"
 #include "challenger/challenger_memory.hpp"
@@ -6,12 +8,47 @@
 #include <cstddef>
 #include <cstdlib>
 
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
+
 using namespace challenger;
 
 int main(int argc, char* argv[]) {
+    // SDLのサブシステムを立ち上げる
+    if(!!SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO)) {
+        return EXIT_FAILURE;
+    }
+    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+
     // ウィンドウとレンダラを作成
     const auto MainWindow = Create<Window, SDL_CreateWindow>("Scancode Configure", 300, 300, SDL_WINDOW_OPENGL);
     const auto WindowRenderer = Create<Renderer, SDL_CreateRenderer>(MainWindow.get(), nullptr, SDL_RENDERER_ACCELERATED);
+
+    // ImGuiのセットアップ
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_NavEnableGamepad;
+
+    // ImGuiのスタイルをシステムのテーマに合わせる
+    if (SDL_GetSystemTheme() == SDL_SystemTheme::SDL_SYSTEM_THEME_DARK) {
+        ImGui::StyleColorsDark();
+    } else {
+        ImGui::StyleColorsLight();
+    }
+
+    // ImGuiSDL3向けの初期化を行う
+    ImGui_ImplSDL3_InitForSDLRenderer(MainWindow.get(), WindowRenderer.get());
+    ImGui_ImplSDLRenderer3_Init(WindowRenderer.get());
+
+
+    // ImGuiを閉じる
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    // SDLのサブシステムを閉じる
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
