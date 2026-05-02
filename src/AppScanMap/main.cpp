f963adfeb7553cfbc56a3075b7d46ca0aa7f5a6f
Y-T10 <t06t06ca2@gmail.com>
2026-05-02 20:09:47 +0900

    [add] cliツールのプロジェクトを追加

---
+++ src/AppScanMap/main.cpp
@@ -0,0 +1,15 @@
+#include "CregAdmin.hpp"
+#include "CproHandle.hpp"
+#include "CregHandler.hpp"
+#include "CregTypes.hpp"
+#include "CprocPipe.hpp"
+#include "CsmCodec.hpp"
+#include "CsmViewer.hpp"
+
+#include <cstdint>
+#include <cstdio>
+#include <vector>
+
+int main() {
+    return 0;
+}
