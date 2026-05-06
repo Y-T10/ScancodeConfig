db35ab26bc49f08db0b71671bba14d6eab9f0181
Y-T10 <t06t06ca2@gmail.com>
2026-05-07 01:35:42 +0900

    [add] レジストリの中身を表示する処理を追加

--- src/AppScanMap/main.cpp
+++ src/AppScanMap/main.cpp
@@ -1,15 +1,114 @@
-#include "CregAdmin.hpp"
-#include "CproHandle.hpp"
-#include "CregHandler.hpp"
-#include "CregTypes.hpp"
-#include "CprocPipe.hpp"
-#include "CsmCodec.hpp"
-#include "CsmViewer.hpp"
-
-#include <cstdint>
 #include <cstdio>
+#include <expected>
+#include <format>
+#include <print>
 #include <vector>
 
-int main() {
+#include "CregHandler.hpp"
+#include "CsmCodec.hpp"
+
+std::expected<std::string, DWORD> key_name(
+    const CompScanMap::Scancode code) noexcept;
+std::string utf16_to_utf8(const std::wstring& str) noexcept;
+void print_scancode_map() noexcept;
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        print_scancode_map();
+        return 0;
+    }
+
     return 0;
+}
+
+void print_scancode_map() noexcept {
+    using namespace CompScanMap;
+    using namespace CompReg;
+
+    constexpr auto EntryName = TEXT("Scancode Map");
+    constexpr auto KeyPath =
+        TEXT("SYSTEM\\CurrentControlSet\\Control\\Keyboard Layout");
+
+    const auto Handle =
+        OpenRegKey(HKEY_LOCAL_MACHINE, KeyPath, KEY_QUERY_VALUE);
+    if (!Handle) {
+        std::println(stderr, "failed to open registry key");
+        return;
+    }
+
+    const auto Keymapping = ReadKeyValueBin(Handle, EntryName);
+    if (!Keymapping) {
+        std::println(stderr, "failed to read entry value");
+        return;
+    }
+
+    const auto CurrentMap = DecodeScancodeMap(*Keymapping);
+    if (!CurrentMap) {
+        std::println(stderr, "failed to decode binary");
+        return;
+    }
+
+    const auto PrintError = [](const DWORD& v) {
+        return std::format("error:{:#08x}", v);
+    };
+
+    std::println("{:<.20s}    {:<.20s}", "keyboard input", "conversion result");
+    std::println("{:-<44.44s}", "");
+    for (const auto& pair : *CurrentMap) {
+        const auto fromName = key_name(pair.from).transform_error(PrintError);
+        const auto toName = key_name(pair.to).transform_error(PrintError);
+        std::println("{:<#04x} ({:<15.15s}) -> {:<#04x} ({:<15.15s})",
+                     pair.from, fromName ? fromName.value() : fromName.error(),
+                     pair.to, toName ? toName.value() : toName.error());
+    }
+}
+
+std::string utf16_to_utf8(const std::vector<wchar_t>& str) noexcept {
+    if (str.empty()) {
+        return "";
+    }
+    const int size = WideCharToMultiByte(CP_UTF8, 0, str.data(), str.size(),
+                                         nullptr, 0, nullptr, nullptr);
+    if (size == 0) {
+        return "";
+    }
+    std::vector<char> output(size, '\0');
+    if (0 == WideCharToMultiByte(CP_UTF8, 0, str.data(), str.size(),
+                                 output.data(), output.size() * sizeof(wchar_t),
+                                 nullptr, nullptr)) {
+        return "";
+    }
+    return std::string(output.begin(), output.end() - 1);
+};
+
+std::expected<std::string, DWORD> key_name(
+    const CompScanMap::Scancode code) noexcept {
+    constexpr auto NoumNamelessKey = "no name";
+
+    if (code == 0x0000) {
+        return "null";
+    }
+
+    const LONG Value =
+        ((code & 0xff00) != 0 ? 0x1000000 : 0) | (0xff0000 & (code << 16));
+    auto buffer = std::vector<wchar_t>(128, L'\0');
+    const int NameLength = GetKeyNameTextW(Value, buffer.data(), buffer.size());
+    if (NameLength == 0) {
+        const auto Code = GetLastError();
+        if (Code == ERROR_SUCCESS) {
+            return NoumNamelessKey;
+        }
+        return std::unexpected{Code};
+    }
+    buffer.resize(NameLength);
+    buffer.emplace_back(L'\0');
+    if (0 == GetKeyNameTextW(Value, buffer.data(), buffer.size())) {
+        const auto Code = GetLastError();
+        if (Code == ERROR_SUCCESS) {
+            return NoumNamelessKey;
+        }
+        return std::unexpected{Code};
+    }
+
+    return utf16_to_utf8(buffer);
 }
